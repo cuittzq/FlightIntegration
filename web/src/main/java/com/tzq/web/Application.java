d5b82fbe7f2893d3361e64ba6bd409dd8bc714c0
Mr.Tan <tzq24955@ly.com>
2018-05-04 17:37:41 +0800

    整合mybatis generator 自动生成mapper和model

--- web/src/main/java/com/tzq/web/Application.java
+++ web/src/main/java/com/tzq/web/Application.java
@@ -1,5 +1,6 @@
 package com.tzq.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -15,8 +16,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * LY.com Inc.
  * Copyright (c) 2004-2017 All Rights Reserved.
  */
-@SpringBootApplication(scanBasePackages="com.tzq")
+@SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages={"com.tzq"})
+@MapperScan("com.tzq.dal.mapper")
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
