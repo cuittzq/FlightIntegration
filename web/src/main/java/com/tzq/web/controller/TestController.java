4e1ddfde789b4d9f7e112823d242a9745198d195
tanzhiqiang <tzq24955@ly.com>
2018-05-08 22:02:10 +0800

    测试查询接口

--- web/src/main/java/com/tzq/web/controller/TestController.java
+++ web/src/main/java/com/tzq/web/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.tzq.web.controller;
 
+import com.tzq.biz.common.enums.TripTypeEnum;
+import com.tzq.biz.common.model.context.RouteContext;
+import com.tzq.biz.common.model.integration.FlightRouteVO;
+import com.tzq.biz.common.model.integration.SearchVO;
+import com.tzq.biz.service.core.SearchFlightService;
 import com.tzq.dal.model.Test;
 import com.tzq.dal.service.UserService;
+import com.tzq.integration.utils.DateUtils;
 import com.tzq.web.aop.UserAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
 import java.util.Date;
 
 /**
@@ -25,6 +32,9 @@ public class TestController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private SearchFlightService lccIntlSearchFlightService;
+
     @RequestMapping(value = "/gethello", method = RequestMethod.GET)
     @UserAccess(desc = "gethello")
     public String gethello() {
@@ -33,6 +43,27 @@ public class TestController {
         user.setName("test");
         user.setUpdatetime(new Date());
         userService.addUser(user);
+        return "hello";
+    }
+
+    @RequestMapping(value = "/searchFlight", method = RequestMethod.GET)
+    @UserAccess(desc = "searchFlight")
+    public String searchFlight() {
+        SearchVO searchVO = new SearchVO();
+        searchVO.setDepAirportCode("PEK");
+        searchVO.setArrAirportCode("CTU");
+        searchVO.setTripType(TripTypeEnum.OW);
+        try {
+            searchVO.setDepDate(DateUtils.parseDateNoTime("20180825"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        String areaType = "1";
+        String airlineCode = "LCC";
+        String sourceId = "10001";
+        RouteContext<SearchVO> context = new RouteContext<>(searchVO, areaType, airlineCode, sourceId);
+        FlightRouteVO flightRouteVO = lccIntlSearchFlightService.searchFlight(context);
         return "hello";
     }
 }
