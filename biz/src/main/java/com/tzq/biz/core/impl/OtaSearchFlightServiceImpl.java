3441b62754e41ede450166f315af76238a664545
tanzhiqiang <tzq24955@ly.com>
2018-05-17 22:53:52 +0800

    查询接口数据模型转化

--- biz/src/main/java/com/tzq/biz/core/impl/OtaSearchFlightServiceImpl.java
+++ biz/src/main/java/com/tzq/biz/core/impl/OtaSearchFlightServiceImpl.java
@@ -24,7 +24,6 @@ public class OtaSearchFlightServiceImpl implements OtaSearchFlightService {
     @Resource
     PurchaseProxy purchaseProxy;
 
-
     @Override
     public SingleResult<FlightRouteVO> searchFlight(RouteContext<SearchVO> context) {
         Assert.notNull(context, "RouteContext can not be null ,searchFlight failure");
