76feff27fb089fdf0eefdfcbfad1993df24f7245
cl24957 <cl24957@ly.com>
2018-05-21 10:24:26 +0800

    下单异常处理

--- biz/src/main/java/com/tzq/biz/core/impl/OtaCreateOrderServiceImpl.java
+++ biz/src/main/java/com/tzq/biz/core/impl/OtaCreateOrderServiceImpl.java
@@ -41,15 +41,14 @@ public class OtaCreateOrderServiceImpl implements OtaCreateOrderService {
         String purchaseEnum =  context.getT().getRoutings().getData().get(OtaConstants.PURCHANAME).toString();
         context.setPurchaseEnum(PurchaseEnum.getEnumByCode(purchaseEnum));
         SingleResult<CreateOrderResVO> response = null;
-        CreateOrderResVO verifyResVO = purchaseProxy.createOrder(context);
         try {
+            CreateOrderResVO verifyResVO = purchaseProxy.createOrder(context);
             if (verifyResVO == null) {
                 response = new SingleResult<>(verifyResVO, false, CommonExcetpionConstant.SYSTEM_EXCEPTION_CODE, "无数据");
                 return response;
             }
             response = new SingleResult<>(verifyResVO, true, CommonExcetpionConstant.SUCCESS, "");
-        } catch (
-                Exception ex) {
+        } catch ( Exception ex) {
             if (ex instanceof ServiceAbstractException) {
                 response = new SingleResult<>(null, false, ((ServiceAbstractException) ex).getErrorCode(), ex.getMessage());
             } else {
